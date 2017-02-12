58a0aa00710689405ca20f92c4c194a073c5b713
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2017-02-12 09:11:47 -0800

    Make URL use right item

--- display/d_txid.py
+++ display/d_txid.py
@@ -25,12 +25,20 @@ def display_txid(txid=None, dbid=None):
 	this_endpoint = g.config_items["self"]["url"] + "/api/txid/" + call_var
 	#this_endpoint = api_txid(txid=txid, dbid=dbid)
 	this_attempts_endpoint = g.config_items["self"]["url"] + "/api/attempts/" + call_var
+	
+	print(this_endpoint)
+	print(this_attempts_endpoint)
 			
 	try: 
 		txid_data = requests.get(this_endpoint).content
 		txid_attempts = requests.get(this_attempts_endpoint).content
 	except Exception as e:
 		print(str(e))
+	
+	print(type(txid_data))
+	print(txid_data)
+	print(type(txid_attempts))
+	print(txid_attempts)
 	
 	stringified = txid_data.decode('utf-8')
 	stringified_attempts = txid_attempts.decode('utf-8')
