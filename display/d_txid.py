67c2891b9e702ae5bfa062463dde37f1701f52f9
Chris Halbersma <chris.halbersma@verizondigitalmedia.com>
2017-02-11 22:38:50 -0800

    Attempts in Dtxid

--- display/d_txid.py
+++ display/d_txid.py
@@ -13,16 +13,20 @@ def display_txid(txid=None, dbid=None):
 	error_dict = dict()
 		
 	this_endpoint = "http://127.0.0.1:8080" + "/api/txid/" + str(txid)
+	this_attempts_endpoint = "http://127.0.0.1:8080" + "/api/attempts/" + str(txid)
 	print(this_endpoint)
 		
 	# Grab Endpoint
 	try: 
 		txid_data = requests.get(this_endpoint).content
+		txid_attempts = requests.get(this_attempts_endpoint).content
 	except Exception as e:
 		print(str(e))
 	
 	stringified = txid_data.decode('utf-8')
+	stringified_attempts = txid_data.decode('utf-8')
 	sanitized = json.loads(stringified)
+	sanitized_attempts = json.loads(stringified_attempts)
 	
-	return render_template('display/Dtxid.html.jinja', data=sanitized)
+	return render_template('display/Dtxid.html.jinja', data=sanitized, attempts=sanitized_attempts)
 
