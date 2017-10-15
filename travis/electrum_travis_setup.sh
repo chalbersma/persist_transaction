50c37636c4ca21ce7de9cfcff4127a42722b0da7
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 16:21:37 -0700

    Adding test wallet (don't use this for actual transactions).

--- travis/electrum_travis_setup.sh
+++ travis/electrum_travis_setup.sh
@@ -6,8 +6,11 @@ sudo apt-get install python-qt4 python-pip
 # Install Electrum
 sudo pip2 install ./travis/sources/Electrum-2.9.3.tar.gz
 
-# Start Electrum Daemon
-electrum daemon start
+# Create a Blank wallet
+electrum create
 
-# Check to see if Electrum is doing it's thing right
+# Start Electrum Daemon
+electrum -w ./travis/shitty_test_wallet daemon start
+
+# Check to see if Electrum is doing it's thing righ
 electrum is_synchronized
