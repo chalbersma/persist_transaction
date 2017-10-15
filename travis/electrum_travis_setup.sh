c604421c3fb6ca09cfa8289b0b5871a4b5410e8c
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 16:45:29 -0700

    electrum

--- travis/electrum_travis_setup.sh
+++ travis/electrum_travis_setup.sh
@@ -7,6 +7,7 @@ sudo apt-get install python-qt4 python-pip
 sudo pip2 install ./travis/sources/Electrum-2.9.3.tar.gz
 
 # Start Electrum Daemon
+
 electrum -w ./travis/electrum/shitty_test_wallet daemon start
 
 	# Note, the above uses a test wallet. That wallet
