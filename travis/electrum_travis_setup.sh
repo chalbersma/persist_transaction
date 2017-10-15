2813b82e063cf005c0b0ab235a3127b1cc122e63
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 16:02:36 -0700

    Adding comments on travis scripts, adding electrum startup.

--- travis/electrum_travis_setup.sh
+++ travis/electrum_travis_setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Install Electrum Dependencies
 sudo apt-get install python-qt4 python-pip
 
+# Install Electrum
 sudo pip2 install ./travis/sources/Electrum-2.9.3.tar.gz
+
+# Start Electrum Daemon
+electrum daemon start
+
+# Check to see if Electrum is doing it's thing right
+electrum is_synchronized
