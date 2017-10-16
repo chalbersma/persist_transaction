66a078ec17871435b8b4dcb7e7c7667236e52b1d
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 19:18:05 -0700

    api_pid

--- travis/percy_api_travis_setup.sh
+++ travis/percy_api_travis_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./api.py -c ./travis/config_travis.ini 
+# Start API (And send it to background).
+./api.py -c ./travis/config_travis.ini  &
 
+api_pid=$!
+
+echo -e "API Running with PID of ${api_pid}"
 
