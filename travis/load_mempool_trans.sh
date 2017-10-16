aed585e65ad69189ba3c71014c59fcfeb3b1db30
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 19:45:31 -0700

    Moving load_mempool_trans.sh test script to travis Dir

--- travis/load_mempool_trans.sh
+++ travis/load_mempool_trans.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 # Load Test Transaction
 
 for transaction in $(curl -s https://blockchain.info/unconfirmed-transactions?format=json | jq '.txs[].hash' | tr -s '"' ' ' | xargs); do
 	echo ${transaction}
 	curl -Ls localhost:8080/api/addtrans/${transaction}
 done
+
+# Add DB Test Information Here
