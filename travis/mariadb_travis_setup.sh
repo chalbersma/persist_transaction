095e953329c2e78acf1f2047236fcb201218bd46
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-15 18:02:36 -0700

    Add schema to mariadb setup.

--- travis/mariadb_travis_setup.sh
+++ travis/mariadb_travis_setup.sh
@@ -4,3 +4,9 @@ set -x
 
 # Setup The MariaDB Stuff for testing
 service mysql status
+
+# Sets up database
+# Sets up users persist & persist_perf with the default
+# super duper insecure passwords.
+cat setup/setup.sql | mysql 
+
