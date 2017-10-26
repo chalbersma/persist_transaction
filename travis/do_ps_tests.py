c31a8e3eadede064ac6ff53ce20ea1f09c6e2ca9
Christopher R. Halbersma <chalbersma.12@gmail.com>
2017-10-25 21:30:59 -0700

    More Travis Stuff

--- travis/do_ps_tests.py
+++ travis/do_ps_tests.py
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-
-# Load in Some Random Transactions
-./travis/load_mempool_trans.sh
-
 # Process those transactions
 ./process_transactions.py -c ./travis/config_travis.ini -V 
-
-# Test Database Arrival
-
-# API Tests Here : 
 
